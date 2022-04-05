708721d81e39925aa85ef89d48bf8fab04c39d88
Admin <kadqwq@gmail.com>
2022-04-05 08:51:24 +0300

    Initial commit

---
+++ OnlineTestingProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineTestingProject.Startup))]
+namespace OnlineTestingProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
